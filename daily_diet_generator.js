48e30a25147d2787f141ae5a96055792132d2c4a
Gergo <czimmermann.gergo97@gmail.com>
2021-11-28 17:53:41 +0100

    random Array Element function development, skipped steps to check the results

---
+++ daily_diet_generator.js
@@ -0,0 +1,11 @@
+
+
+// choose a random element from an array
+const randomArrayElement = array =>
+{
+    return array[Math.floor(Math.random()*array.length)];
+}
+//let trialArray = ["alma", "béta","gamma","kappa"];
+//console.log(randomArrayElement(trialArray)); // check if we can select a random element
+
+//------------------------------------------------------------------------------------------
