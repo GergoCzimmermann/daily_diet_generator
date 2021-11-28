33fbfd5ed9b62002105da7b3bff3a151f67592e1
Gergo <czimmermann.gergo97@gmail.com>
2021-11-28 19:37:17 +0100

    Readme general info update / daily diet generator functionality development, with only 2 meals/ catering

--- daily_diet_generator.js
+++ daily_diet_generator.js
@@ -9,3 +9,43 @@ const randomArrayElement = array =>
 //console.log(randomArrayElement(trialArray)); // check if we can select a random element
 
 //------------------------------------------------------------------------------------------
+// List all the foods
+const foodsObject = {
+    breakfast: ["tojásrántotta", "zabkása"],
+    tenHourLunch: ["alma","keksz"],
+    lunch: ["rántotthús bulgurral","csilisbab"],
+    snack: ["sajtos zabkeksz", "kókuszos zabkeksz"],
+    dinner: ["tonhalsaláta", "virsli"]
+}
+//console.log(foodsObject.breakfast);
+
+//------------------------------------------------------------------------------------------
+// creat a random daily diet based on the input food list
+const randomDietGenerator= foods => {
+    const dailyDietObj = {
+        breakfast: "",
+        tenHourLunch: "",
+        lunch: "",
+        snack: "",
+        dinner: ""
+    }
+
+    dailyDietObj.breakfast = randomArrayElement(foods.breakfast);
+    dailyDietObj.tenHourLunch = randomArrayElement(foods.tenHourLunch);
+    dailyDietObj.lunch = randomArrayElement(foods.lunch);
+    dailyDietObj.snack = randomArrayElement(foods.snack);
+    dailyDietObj.dinner = randomArrayElement(foods.dinner);
+
+    return dailyDietObj;
+}
+
+//------------------------------------------------------------------------------------------
+// console.log(randomDietGenerator(foodsObject)); // test the diet generator
+
+const dailyDiet= randomDietGenerator(foodsObject);
+console.log("Szia! Örülök, hogy látlak! A napi étrend a következő:")
+console.log("Reggeli: " + dailyDiet.breakfast);
+console.log("Tízórai: " + dailyDiet.tenHourLunch);
+console.log("Ebéd: " + dailyDiet.lunch);
+console.log("Uzsonna: " + dailyDiet.snack);
+console.log("Vacsora: " + dailyDiet.dinner);
--- readme.md
+++ readme.md
@@ -4,5 +4,5 @@
 * General Information
 
 ## General Information
-Every time we run the generator, a random daily diet will be appeared.
+Every time we run the generator, a random daily diet will appeare. Based on the input foods list the program select a random meal for the actual catering.
 
